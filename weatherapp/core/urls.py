9d22eb43f5ca0b27c1293fa4b64c7af8fa5c3cf8
nyx500 <45918281+nyx500@users.noreply.github.com>
2021-04-23 13:43:23 +0200

    Made user login pages

--- weatherapp/core/urls.py
+++ weatherapp/core/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('get_data', views.get_data, name='get_data'),
-    path('city/<str:city>/', views.get_city, name='get_city')
+    path('city/<str:city>/', views.get_city, name='get_city'),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
 ]
--- weatherapp/core/views.py
+++ weatherapp/core/views.py
@@ -1,10 +1,66 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
 import requests
 from bs4 import BeautifulSoup
 import json
-from .functions import *
 from django.views.decorators.csrf import csrf_exempt
+from .functions import *
+from .models import *
+
+def index(request):
+    return render(request, 'core/index.html')
+
+def login_view(request):
+    if request.method == "POST":
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "core/login.html", {
+                "message": "Invalid email and/or password."
+                })
+    else:
+        return render(request, "core/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        username = request.POST["user"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "core/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "core/register.html", {
+                "message": "Email address and/or username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "core/register.html")
 
 @csrf_exempt
 def get_data(request):
@@ -36,7 +92,3 @@ def get_city(request, city):
         return render(request, 'core/index.html', {
             'data': weather_data
         })
-
-
-def index(request):
-    return render(request, 'core/index.html')
