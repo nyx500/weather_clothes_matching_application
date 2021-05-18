855f9cb1a2f293b67f2490346a93028d3e91c0ef
nyx500 <45918281+nyx500@users.noreply.github.com>
2021-05-18 17:26:26 +0200

    Added no-image link image

--- weatherapp/core/views.py
+++ weatherapp/core/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-import requests
-from bs4 import BeautifulSoup
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .functions import *
@@ -50,39 +48,43 @@ def register(request):
                 "message": "Passwords must match."
             })
 
+        # Checks if email address in use
+        email_addresses = []
+        for user in User.objects.all():
+            email_addresses.append(user.email)
+        
+        if email in email_addresses:
+            return render(request, "core/register.html", {
+                "message": "Email-address already taken."
+            })
+
         # Attempt to create new user
         try:
             user = User.objects.create_user(username, email, password)
             user.save()
-        except IntegrityError as e:
-            print(e)
+        except IntegrityError:
             return render(request, "core/register.html", {
-                "message": "Email address and/or username already taken."
+                "message": "Username already taken."
             })
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "core/register.html")
 
+# Processes the user's input for the location they want to look up the weather for
 @csrf_exempt
 def get_data(request):
     if request.method != "POST":
         return JsonResponse({"error": "POST request required."}, status=400)
     else:
+        # Gets data from user's form
         data = json.loads(request.body)
-        print(data)
         city = data.get("city", "")
-        global time
         time = data.get("time", "")
-        global units
         units = data.get("units", "")
         if city != 'no_city':
-            print(f'City: {city}')
-            print(f"Time: {time}")
-            print(f"Units: {units}")
             html_content = get_html_content(city, time)
             weather_data = get_weather_data(html_content, units)
-            print(f'Weather data: {weather_data}')
             if weather_data == 'No such city':
                 return JsonResponse({"Error": "This location input is invalid"})
             else:
@@ -96,8 +98,9 @@ def get_data(request):
             print(f'None city: {city}');
             return render(request, 'core/index.html')
 
+# If tge user refreshes page on the city JS state, then this allows Python to send a hidden message to index page which is waited for on the script as a signal to fetchthe weather data from get_data and the API again
 def get_city(request, city):
-    print(f"CITY ROUTE: {city}")
+    print("OK")
     return render(request, 'core/index.html', {
         'get_data': "yes"
     })
@@ -108,7 +111,6 @@ def recipes(request):
     })
 
 
-
 @login_required
 def submit(request):
     if request.method == "POST":
@@ -116,14 +118,17 @@ def submit(request):
         if form.is_valid():
             data = form.cleaned_data
             url = data["recipe"]
-            image_url = data["image"]
+            if data["image"]:
+                image_url = data["image"]
+            else:
+                image_url = "https://img.icons8.com/color/96/000000/no-image.png"
             print(f"URL: {url}")
-            # Pretends to be a browser to check access to the URL
+            # Pretends to be a browser agent on Mozilla to check access to the URL via the link for the recipe and image input, to see that the URL the user has entered is not a 404/3 error
             request_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
             recipe_request = urllib.request.Request(url, headers = request_headers)
             image_request = urllib.request.Request(image_url, headers=request_headers)
 
-            # Validating URL
+            # Validates the recipe URL
             try:
                 recipe_response = urllib.request.urlopen(recipe_request)
                 status_recipe = recipe_response.getcode()
@@ -132,42 +137,32 @@ def submit(request):
                     'message': 'Error: Invalid URL.'
                 })
 
-            # Validating Image URL
+            # Validates Image URL
+            # Checks if image url has been inputted by the user
             if len(image_url) > 0:
                 try:
                     image_response = urllib.request.urlopen(image_request)
                     status_image = urllib.request.urlopen(image_request).getcode()
                     message = str(image_response.info())
-
+                    # Parses the information that comes on the image link to see if the file is really an image
                     for line in message.splitlines():
-                        print(f"Line: {line}")
                         if 'Content-Type' in line and 'image' in line:
-                            print("Image URL is valid")
                             try:
                                 new_recipe = Recipe()
-                                print(request.user)
                                 new_recipe.user = request.user
-                                print(data["title"])
                                 title = data["title"].title()
                                 new_recipe.title = title
-                                print(data["description"])
                                 new_recipe.description = data["description"]
-                                print(url)
                                 new_recipe.recipe = url
-                                print(image_url)
                                 new_recipe.image = image_url
-                                print(data["food_type"])
                                 new_recipe.food_type = data["food_type"]
-                                print(data["diets"])
                                 new_recipe.diets = data["diets"]
-                                print(data["meals"])
                                 new_recipe.meals = data["meals"]
                                 new_recipe.save()
                                 for weather in data["weather"]:
                                     new_recipe.weather.add(weather)
                                 return HttpResponseRedirect(reverse("recipes"))
                             except:
-                                print("Did not save the form data")
                                 return render(request, "core/index.html", {
                                     'message': 'Error: Could not save form.'
                                 })
@@ -175,6 +170,21 @@ def submit(request):
                     return render(request, "core/index.html", {
                     'message': 'Error: Invalid image URL.'
                 })
+            else:
+                new_recipe = Recipe()
+                new_recipe.user = request.user
+                title = data["title"].title()
+                new_recipe.title = title
+                new_recipe.description = data["description"]
+                new_recipe.recipe = url
+                new_recipe.image = "https://img.icons8.com/color/96/000000/no-image.png"
+                new_recipe.food_type = data["food_type"]
+                new_recipe.diets = data["diets"]
+                new_recipe.meals = data["meals"]
+                new_recipe.save()
+                for weather in data["weather"]:
+                    new_recipe.weather.add(weather)
+                return HttpResponseRedirect(reverse("recipes"))
 
             if not valid_url_extension(image_url):
                 print("Not a valid url extension")
